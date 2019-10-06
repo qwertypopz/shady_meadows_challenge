7ab7c448b0dde45edbe78b77dd8e42731461f119
Qwerty <lost411@gmail.com>
2019-10-05 22:49:35 -0400

    Added test_cases to test.py

--- test.py
+++ test.py
@@ -2,21 +2,36 @@
 Make reservations
 '''
 import common_functions
+import contact
 import reservation
+import splash
 import sys
 import datetime
 
-def single(log):
+
+def splash_single_now_2(log):
     '''
-    Create a reservation for a Single room on a specified date and duration
+    Create a reservation for a Single room today for two days
     '''
     args = {
         "room":"Single",
         "start_date":datetime.datetime.now(),
-        "duration":3
+        "duration":2
     }
+    splash.validate(log)
     reservation.action(args, log)
 
+def contact_twin_thanksgiving_3(log):
+    '''
+    Create a reservation for a Twin room for Thanksgiving for three days
+    '''
+    args = {
+        "room":"Twin",
+        "start_date":datetime.datetime(2019, 11, 27),
+        "duration":3
+    }
+    contact.action(log)
+    reservation.action(args, log)
 
 if __name__ == '__main__':
     log = common_functions.setup_log()
